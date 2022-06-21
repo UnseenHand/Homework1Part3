c3592532717d770e377269942499827f139950aa
UnseenHand <95188996+UnseenHand@users.noreply.github.com>
2022-06-21 19:04:12 +0300

    Homework1Part3 v.1.0
    "area2" was replaced as "area"

--- Homework1Part3/Program.cs
+++ Homework1Part3/Program.cs
@@ -11,9 +11,9 @@ namespace Homework1Part3
             Console.Write("The radius of the circle : ");
             double r = Convert.ToDouble(Console.ReadLine());
             double length = 2 * PI * r;
-            double area2 = PI * r * r;
+            double area = PI * r * r;
             double volume = 4 / 3 * PI * r * r * r;
-            Console.WriteLine($"The length of the circle : {length}, its area : {area2}, and the volume : {volume}");
+            Console.WriteLine($"The length of the circle : {length}, its area : {area}, and the volume : {volume}");
         }
     }
 }
