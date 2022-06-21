5266c9b18024eb76c293167a5b3d41b2f7e3354d
Vitaliy Ugera <shnobel12@gmail.com>
2022-06-21 18:59:32 +0300

    Добавьте файлы проекта.

---
+++ Homework1Part3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework1Part3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+
+            Console.Write("The radius of the circle : ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double length = 2 * PI * r;
+            double area2 = PI * r * r;
+            double volume = 4 / 3 * PI * r * r * r;
+            Console.WriteLine($"The length of the circle : {length}, its area : {area2}, and the volume : {volume}");
+        }
+    }
+}
